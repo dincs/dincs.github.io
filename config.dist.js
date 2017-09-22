43e854618797d8b3f6cd07a87bdac85045f0a950
Burhanuddin Bin Khalid <artixrilex@gmail.com>
2017-09-22 16:52:46 +0800

    Update config.dist.js

--- config.dist.js
+++ config.dist.js
@@ -2,6 +2,6 @@ var config = {
   firebase: 'real-time-tracking-bcce8',
   mapbox: {
       accessToken: 'pk.eyJ1IjoicmlsZXhicmFkZXIiLCJhIjoiY2ozMDAxdnBmMDAzNTMzanMyMjJ5bTZsZyJ9.1KZbtlEhO7mfSELugWCqxA',
-      mapId: 'mapbox.light',
+      mapId: 'mapbox.streets',
   }
 }
