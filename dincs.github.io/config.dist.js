dbedb4e431bae36d4919740e865ba95527d68950
dincs <artixrilex@gmail.com>
2017-09-28 17:09:55 +0800

    deleting folder

--- dincs.github.io/config.dist.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  firebase: 'real-time-tracking-bcce8',
-  mapbox: {
-      accessToken: 'pk.eyJ1IjoicmlsZXhicmFkZXIiLCJhIjoiY2ozMDAxdnBmMDAzNTMzanMyMjJ5bTZsZyJ9.1KZbtlEhO7mfSELugWCqxA',
-      mapId: 'mapbox.light',
-  }
-}
